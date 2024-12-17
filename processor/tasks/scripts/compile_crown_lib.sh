c863435c618a05ad993e03e560ced440cbb67946
tvoigtlaender <tim.voigtlaender@kit.edu>
2024-12-17 16:11:46 +0100

    Add curly brackets to all bash variables to prevent issues with uncertain variable names

--- processor/tasks/scripts/compile_crown_lib.sh
+++ processor/tasks/scripts/compile_crown_lib.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
-CROWNFOLDER=$1
-INSTALLDIR=$2
-BUILDDIR=$3
-ANALYSIS=$4
+CROWNFOLDER=${1}
+INSTALLDIR=${2}
+BUILDDIR=${3}
+ANALYSIS=${4}
 
-echo "Crown folder: $CROWNFOLDER"
-echo "Install dir: $INSTALLDIR"
-echo "Build dir: $BUILDDIR"
+echo "Crown folder: ${CROWNFOLDER}"
+echo "Install dir: ${INSTALLDIR}"
+echo "Build dir: ${BUILDDIR}"
 # setup with analysis clone if needed
 set -o pipefail
 set -e
-source $ANALYSIS_PATH/CROWN/init.sh $ANALYSIS
-# remove conda prefix from $PATH so cmakes uses the LCG stack python and not the conda one
+source ${ANALYSIS_PATH}/CROWN/init.sh ${ANALYSIS}
+# remove conda prefix from ${PATH} so cmakes uses the LCG stack python and not the conda one
 if [[ ! -z "${CONDA_PREFIX}" ]]; then
-	PATH=$(echo $PATH | sed "s@$CONDA_PREFIX@@g")
-	# PATH=$(echo $PATH | sed 's%/cvmfs/etp.kit.edu/[^:]*:%%g')
+	PATH=$(echo ${PATH} | sed "s@${CONDA_PREFIX}@@g")
+	# PATH=$(echo ${PATH} | sed 's%/cvmfs/etp.kit.edu/[^:]*:%%g')
 	CONDA_PYTHON_EXE=""
 	CONDA_EXE=""
 	CONDA_PREFIX=""
@@ -22,23 +22,23 @@ fi
 # use a fourth of the machine for compiling
 THREADS_AVAILABLE=$(grep -c ^processor /proc/cpuinfo)
 THREADS=$(( THREADS_AVAILABLE / 4 ))
-echo "Using $THREADS threads for the compilation"
+echo "Using ${THREADS} threads for the compilation"
 which cmake
 
-if cmake $CROWNFOLDER \
+if cmake ${CROWNFOLDER} \
 	 -DBUILD_CROWNLIB_ONLY=ON \
-	 -DINSTALLDIR=$INSTALLDIR \
-	 -DANALYSIS=$ANALYSIS \
-	 -B$BUILDDIR 2>&1 |tee $BUILDDIR/cmake.log; then
+	 -DINSTALLDIR=${INSTALLDIR} \
+	 -DANALYSIS=${ANALYSIS} \
+	 -B${BUILDDIR} 2>&1 |tee ${BUILDDIR}/cmake.log; then
 echo "CMake finished successfully"
 else
 	echo "-------------------------------------------------------------------------"
-	echo "CMake failed, check the log file $BUILDDIR/cmake.log for more information"
+	echo "CMake failed, check the log file ${BUILDDIR}/cmake.log for more information"
 	echo "-------------------------------------------------------------------------"
 	sleep 0.1 # wait for the log file to be written
 	exit 1
 fi
-cd $BUILDDIR
+cd ${BUILDDIR}
 echo "Finished preparing the compilation and starting to compile"
-make install -j $THREADS 2>&1 |tee $BUILDDIR/build.log
+make install -j ${THREADS} 2>&1 |tee ${BUILDDIR}/build.log
 echo "Finished the compilation crownlib build successfully"
--- scripts/python-formatting.sh
+++ scripts/python-formatting.sh
@@ -5,16 +5,16 @@ FOUND_ISSUE=0
 for FILENAME in $(find . -path ./law -prune -o -name "*.py");
 do
     # only run the check if the filename ends with .py
-    if [[ $FILENAME == *.py ]]; then
-        echo "Checking $FILENAME"
-        black --check $FILENAME
+    if [[ ${FILENAME} == *.py ]]; then
+        echo "Checking ${FILENAME}"
+        black --check ${FILENAME}
         RETURN_VALUE=$?
-        if [ $RETURN_VALUE -ne 0 ]
+        if [ ${RETURN_VALUE} -ne 0 ]
         then
-            black --diff $FILENAME 2> /dev/null
+            black --diff ${FILENAME} 2> /dev/null
             FOUND_ISSUE=1
         fi
     fi
 done
 
-exit $FOUND_ISSUE
+exit ${FOUND_ISSUE}
