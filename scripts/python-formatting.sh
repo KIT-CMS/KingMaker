5f816ad8537848b9e83f62bc6705d45f6bf30b3f
nshadskiy <51320024+nshadskiy@users.noreply.github.com>
2025-04-29 19:55:34 +0200

    Update python-formatting.sh

--- scripts/python-formatting.sh
+++ scripts/python-formatting.sh
@@ -2,7 +2,7 @@
 
 FOUND_ISSUE=0
 
-for FILENAME in $(find . -path ./law -prune -o -name "*.py"); do
+for FILENAME in $(find . \( -path ./venv -o -path ./law \) -prune -o -name "*.py"); do
 	# only run the check if the filename ends with .py
 	if [[ ${FILENAME} == *.py ]]; then
 		echo "Checking ${FILENAME}"
